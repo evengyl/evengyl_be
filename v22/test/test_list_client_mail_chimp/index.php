c2ed7718d13603619db26d0ad2774cf679bd2652
evengyl <dark.evengyl@gmail.com>
2016-02-25 08:26:06 +0100

    remise a niveau avant les test primaires

---
+++ v22/test/test_list_client_mail_chimp/index.php
@@ -0,0 +1,35 @@
+
+
+
+<?
+require_once ("Evengyl/fonction.php");
+
+  $test = "matedex@matedex.be,Matedex,Matedex\n
+,,A.A.G.
+,,ACCAUTOPARTS-NAMUR
+,,ACCAUTOPARTS-NAMUR
+,,ACCAUTOPARTS-NAMUR
+,,ACTIF PROTECTION
+,BARREMAECKER,ACTIF PROTECTION
+,,AITEC
+,Bert HERMAN,AITEC
+info@aitec.be,Bert HERMAN,AITEC
+,AITEC,AITEC
+,,ALFA PARTS
+guy@alfaparts.be,Guy,ALFA PARTS
+,,FABREX
+,,FABREX
+,,AMEYE BOSSAERT
+,,ARDENNES DIESEL
+ardennediesel@skynet.be,Philippe,ARDENNES DIESEL
+,,ARTON
+,Andre,ARTON
+,,T&A ASSEMAN Electronics
+,,AVIONIC
+,,AVIONIC
+,,AZ - TRONIC";
+
+
+affiche_pre($test);
+
+    ?>
